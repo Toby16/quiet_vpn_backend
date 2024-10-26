ceb2e632bc55035f9465eb33e4432e2a13fca879
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2024-10-26 08:38:12 +0100

    Update routes.py

--- QUIET/routes.py
+++ QUIET/routes.py
@@ -127,6 +127,7 @@ def sign_in(data: signin_User, db: db_dependency):
 
     check_user = db.query(User).filter(User.email == data["email"]).first()
     if check_user is None:
+        return data["email"]
         check_user = db.query(User).filter(User.username == data["email"]).first()
         if check_user is None:
             raise HTTPException(status_code=400, detail="invalid email or password!")
