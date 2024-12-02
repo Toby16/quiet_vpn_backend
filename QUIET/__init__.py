32e821d059532767fc5e18d4a902f23a1afc8040
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2024-12-02 16:52:33 +0100

    Update __init__.py

--- QUIET/__init__.py
+++ QUIET/__init__.py
@@ -4,7 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # List of allowed origins
-origins = ["http://localhost:5173", "https://quiet-vpn-frontend.vercel.app"]
+origins = [
+    "https://security.ghostroute.io",
+    "http://localhost:5173",
+    "https://quiet-vpn-frontend.vercel.app"
+]
 
 app.add_middleware(
     CORSMiddleware,
