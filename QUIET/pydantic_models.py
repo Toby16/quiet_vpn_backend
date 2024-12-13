11cf7d490d88b52514cc431d9c690d2b41c43bca
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2024-12-13 06:41:25 +0100

    Update pydantic_models.py

--- QUIET/pydantic_models.py
+++ QUIET/pydantic_models.py
@@ -51,7 +51,7 @@ class paystack_payment_pydantic_model(BaseModel):
 
 class verify_paystack_payment_pydantic_model(BaseModel):
     username: str = Field(examples=["test_username"])
-    transaction_id: str = Field(examples=["7712579"])
+    transaction_id: str = Field(examples=["vyc5twin3j"])
     days_paid: int = Field(examples=[3])
     server_ip: str = Field(examples=["127.0.0.1"])
     server_location: str = Field(examples=["San Francisco, USA"])
