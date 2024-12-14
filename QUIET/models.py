7642b87a6203ca78c1cfd4d94802b1aa07f70dbc
Baasit Bolaji Ayomiposi <bolajibaasit14@gmail.com>
2024-12-14 16:36:36 +0100

    Update models.py

--- QUIET/models.py
+++ QUIET/models.py
@@ -61,6 +61,7 @@ class transaction(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     trans_id = Column(String(16))  # transaction id
     trans_status = Column(Boolean)  # transaction status
+    expired = Column(Boolean)  # default=Falsee, expired=True, failed/invalid=True
     server_ip = Column(String(20))
     location = Column(Text)
     days_paid = Column(Integer())
